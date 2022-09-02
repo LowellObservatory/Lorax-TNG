4127588e08781c9aed3ca16ed53806b04136f6da
Dyer Lytle <dlytle@lowell.edu>
2022-09-02 10:24:38 -0700

    corrected CameraAgent

--- CameraAgent.py
+++ CameraAgent.py
@@ -14,7 +14,7 @@ class CameraAgent(Agent):
     def handle_message(self):
         print("message: ", end="")
         print(self.current_message.headers["destination"] + ": ", end="")
-        print(ta.current_message.body)
+        print(self.current_message.body)
 
     @abstractmethod
     def status(self):
