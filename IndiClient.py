65cba42a30697990cf34bc27940ae499ee7262f1
Dyer Lytle <dlytle@lowell.edu>
2022-10-29 11:08:52 -0700

    pulled IndiClient out of SBIGComposite subagents

---
+++ IndiClient.py
@@ -0,0 +1,97 @@
+import PyIndi
+import threading
+
+
+class IndiClient(PyIndi.BaseClient):
+    def __init__(self, parent, config):
+        super(IndiClient, self).__init__()
+        self.parent = parent
+        self.config = config
+        self.blobEvent = threading.Event()
+        self.camera = None
+
+    def newDevice(self, d):
+        print("Receiving Device... " + d.getDeviceName())
+        self.device = d
+        self.parent.device = d
+
+    def newProperty(self, p):
+        # print(dir(p))
+        # print("new property " + p.getName() + " for device " + p.getDeviceName())
+        prop_name = p.getName()
+        if prop_name in self.config["status"]:
+            print("storing prop: " + p.getName())
+            self.store_prop(p)
+
+    def removeProperty(self, p):
+        pass
+
+    def newBLOB(self, bp):
+        print("new BLOB ", bp.name)
+        self.blobEvent.set()
+
+    def newSwitch(self, svp):
+        prop_name = svp.name
+        for val in svp:
+            if prop_name in self.config["status"]:
+                self.parent.device_status[val.name] = val.s
+                self.parent.get_status_and_broadcast()
+
+    def newNumber(self, nvp):
+        prop_name = nvp.name
+        for val in nvp:
+            if prop_name in self.config["status"]:
+                self.parent.device_status[val.name] = val.value
+                self.parent.get_status_and_broadcast()
+
+    def newText(self, tvp):
+        prop_name = tvp.name
+        for val in tvp:
+            if prop_name in self.config["status"]:
+                self.parent.device_status[val.name] = val.text
+                self.parent.get_status_and_broadcast()
+
+    def newLight(self, lvp):
+        pass
+
+    def newMessage(self, d, m):
+        pass
+
+    def serverConnected(self):
+        pass
+
+    def serverDisconnected(self, code):
+        pass
+
+    def store_prop(self, prop):
+        prop_name = prop.getName()
+        prop_type = prop.getType()
+        if prop_name in self.config["status"]:
+            if not prop_name in self.parent.device_status:
+                if prop_type == 0:
+                    # Number Type
+                    temp = prop.getNumber()
+                    for val in temp:
+                        if prop_name in self.config["status"]:
+                            self.parent.device_status[val.name] = val.value
+
+                elif prop_type == 1:
+                    # Switch type
+                    temp = prop.getSwitch()
+                    for val in temp:
+                        if prop_name in self.config["status"]:
+                            self.parent.device_status[val.name] = val.s
+
+                elif prop_type == 2:
+                    # Text type
+                    temp = prop.getText()
+                    for val in temp:
+                        if prop_name in self.config["status"]:
+                            self.parent.device_status[val.name] = val.text
+
+                elif prop_type == 3:
+                    # Light type
+                    temp = prop.getLight()
+                    for val in temp:
+                        if prop_name in self.config["status"]:
+                            self.parent.device_status[val.name] = val.text
--- SBIGCcdCooler.py
+++ SBIGCcdCooler.py
@@ -3,138 +3,10 @@ from SubAgent import SubAgent
 import time
 import uuid
 import xmltodict
+import threading
 from datetime import datetime, timezone
 import numpy as np
-
-
-class IndiClient(PyIndi.BaseClient):
-    def __init__(self, parent):
-        super(IndiClient, self).__init__()
-        self.parent = parent
-        status = {}
-
-    def newDevice(self, d):
-        print("Receiving Device... " + d.getDeviceName())
-        self.device = d
-        self.parent.device = d
-
-    def newProperty(self, p):
-        # print(dir(p))
-        # print("new property " + p.getName() + " for device " + p.getDeviceName())
-        # print("type = " + str(p.getType()))
-        # Go store the property in the appropriate status dictionary.
-        device_name = p.getDeviceName()
-        if "CCD" in device_name:
-            self.store_prop(p)
-
-    def removeProperty(self, p):
-        pass
-
-    def newBLOB(self, bp):
-        global blobEvent
-        print("new BLOB ", bp.name)
-        blobEvent.set()
-        pass
-
-    def newSwitch(self, svp):
-        prop_name = svp.name
-        prop_type = 1
-        prop_dict = {"prop_type": prop_type}
-        prop_dict["length"] = len(svp)
-        prop_vals = []
-        for val in svp:
-            prop_vals.append((val.name, val.s))
-        prop_dict["vals"] = prop_vals
-        self.parent.device_status[prop_name] = prop_dict
-
-    def newNumber(self, nvp):
-        prop_name = nvp.name
-        prop_type = 0
-        prop_dict = {"prop_type": prop_type}
-        prop_dict["length"] = len(nvp)
-        prop_vals = []
-        for val in nvp:
-            prop_vals.append((val.name, val.value))
-        prop_dict["vals"] = prop_vals
-        self.parent.device_status[prop_name] = prop_dict
-
-    def newText(self, tvp):
-        prop_name = tvp.name
-        prop_type = 2
-        prop_dict = {"prop_type": prop_type}
-        prop_dict["length"] = len(tvp)
-        prop_vals = []
-        for val in tvp:
-            prop_vals.append((val.name, val.text))
-        prop_dict["vals"] = prop_vals
-        self.parent.device_status[prop_name] = prop_dict
-
-    def newLight(self, lvp):
-        pass
-
-    def newMessage(self, d, m):
-        pass
-
-    def serverConnected(self):
-        pass
-
-    def serverDisconnected(self, code):
-        pass
-
-    def store_prop(self, prop):
-        prop_name = prop.getName()
-        prop_type = prop.getType()
-
-        if not prop_name in self.parent.device_status:
-            if prop_type == 0:
-                # Number Type
-                temp = prop.getNumber()
-                prop_dict = {"prop_type": prop_type}
-                prop_dict["length"] = len(temp)
-                prop_vals = []
-                for val in temp:
-                    # print(dir(val))
-                    prop_vals.append((val.name, val.value))
-                prop_dict["vals"] = prop_vals
-                self.parent.device_status[prop_name] = prop_dict
-                self.parent.get_status_and_broadcast()
-            elif prop_type == 1:
-                # Switch type
-                temp = prop.getSwitch()
-                prop_dict = {"prop_type": prop_type}
-                prop_dict["length"] = len(temp)
-                prop_vals = []
-                for val in temp:
-                    # print(dir(val))
-                    prop_vals.append((val.name, val.s))
-                    # print(val.name)
-                prop_dict["vals"] = prop_vals
-                self.parent.device_status[prop_name] = prop_dict
-            elif prop_type == 2:
-                # Text type
-                temp = prop.getText()
-                # print(len(temp))
-                # print(dir(temp[0]))
-                prop_dict = {"prop_type": prop_type}
-                prop_dict["length"] = len(temp)
-                prop_vals = []
-                for val in temp:
-                    # print(dir(val))
-                    prop_vals.append((val.name, val.text))
-                # print(val.name)
-                prop_dict["vals"] = prop_vals
-                self.parent.device_status[prop_name] = prop_dict
-                # print(status[prop_name])
-            elif prop_type == 3:
-                # Light type
-                temp = prop.getLight()
-                prop_dict = {"prop_type": prop_type}
-                prop_dict["length"] = len(temp)
-                prop_vals = []
-                for val in temp:
-                    prop_vals.append((val.name, val.text))
-                prop_dict["vals"] = prop_vals
-                self.parent.device_status[prop_name] = prop_dict
+from IndiClient import IndiClient
 
 
 class SBIGCcdCooler(SubAgent):
@@ -143,7 +15,7 @@ class SBIGCcdCooler(SubAgent):
         SubAgent.__init__(self, logger, conn, config)
         # Get the host and port for the connection to mount.
         # "config", in this case, is just a dictionary.
-        self.indiclient = IndiClient(self)
+        self.indiclient = IndiClient(self, config)
         # print(self.config)
         self.indiclient.setServer(
             self.config["cooler_host"], self.config["cooler_port"]
@@ -166,9 +38,9 @@ class SBIGCcdCooler(SubAgent):
         c_status = {
             "message_id": uuid.uuid4(),
             "timestamput": datetime.now(timezone.utc),
-            "root": self.device_status,
+            "cooler": self.device_status,
         }
-        status = {"root": c_status}
+        status = {"cooler": c_status}
         xml_format = xmltodict.unparse(status, pretty=True)
 
         print("/topic/" + self.config["outgoing_topic"])
